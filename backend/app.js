513fcf24f7f3ec0e3a4b7aa8b7db6140c7f30173
soreka <ahmed.m.tayah@gmail.com>
2024-07-20 03:44:25 +0300

    initial project structure

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const notificationRoutes = require('./routes/notificationRoutes');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/notifications', notificationRoutes);
+
+module.exports = app;
