79fb6602d2389e2ba14fa7e3d31c68e4635df2dd
cyvber <TahaNurEddin@gmail.com>
2024-07-23 18:57:38 +0300

    comments added and some catch error messages

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,35 +1,44 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// Define the User schema with email and password fields
 const UserSchema = new mongoose.Schema({
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Ensure email is unique
+  },
+  password: {
+    type: String,
+    required: true, // Password is required
+  },
 });
 
-UserSchema.pre('save', async function(next) {
-    try {
-        const salt = bcrypt.genSalt(10)
-        const hash = bcrypt.hash(this.password, salt)
-        this.password = hash;
-        next()
-    } catch (error) {
-        next(error)
+// Pre-save hook to hash the password before saving to the database
+UserSchema.pre('save', async function (next) {
+  try {
+    // Check if the password field is modified
+    if (!this.isModified('password')) {
+      return next();
     }
-})
+    
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
 
-UserSchema.methods.comparePasswords = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
-  
+    next();
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    next(error);
+  }
+});
 
-const User = mongoose.model('User', UserSchema)
-  
-module.exports = User
-  
+// Method to compare entered password with the hashed password in the database
+UserSchema.methods.comparePasswords = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
