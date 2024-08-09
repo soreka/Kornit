a2c2edb2dd9752619a383bef432af29e993cf35d
Aliesa2221 <aliesa059@gmail.com>
2024-08-09 09:22:35 +0300

    auth

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
-const app = require('./app');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const authMiddleware = require('./middleware/auth');
 
-const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/auth', authRoutes);
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.send('This is a protected route.');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
